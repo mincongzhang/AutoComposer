cc47d47877031ed839cf7f1dd58833626ca81934
Mincong Zhang <mincong.zhang@hotmail.com>
2015-03-14 21:01:59 +0000

    prototype done..But a little bit creepy

--- Experiment/runAutoCompose.m
+++ Experiment/runAutoCompose.m
@@ -17,20 +17,34 @@ close all;
 
 %define unary costs : unaryCosts(d,j) represents the cost for having
 %disparity d at pixel j.
-unaryCosts = [2.0 1.1 5.7 1.5 6.0 3.1 ;...
-              0.8 4.8 1.0 3.0 6.9 3.3 ;...
-              4.3 2.3 2.4 2.4 6.6 6.2 ;...
-              6.4 0.0 6.1 0.8 7.1 2.1 ;...
-              2.3 2.2 4.9 8.9 1.0 9.8 ];
+% unaryCosts = [2.0 1.1 5.7 1.5 6.0 3.1 ;...
+%               0.8 4.8 1.0 3.0 6.9 3.3 ;...
+%               4.3 2.3 2.4 2.4 6.6 6.2 ;...
+%               6.4 0.0 6.1 0.8 7.1 2.1 ;...
+%               2.3 2.2 4.9 8.9 1.0 9.8 ];
+
+unaryCosts = abs(randn(20)*1000);
 
 %define pairwise costs:  pairwiseCosts(a,b) represents the cost for changing from 
 %disparity level A at pixel j to disparity level B at pixel j+1;
-pairwiseCosts = [   0   2 100 100 100;...
-                    2   0   2 100 100;...
-                  100   2   0   2 100;...
-                  100 100   2   0   2;...
-                  100 100 100   2   0];
-          
+% pairwiseCosts = [   0   2 100 100 100;...
+%                     2   0   2 100 100;...
+%                   100   2   0   2 100;...
+%                   100 100   2   0   2;...
+%                   100 100 100   2   0];
+pairwiseCosts = ones(size(unaryCosts))*100;
+for i = 3:size(unaryCosts,1)
+    pairwiseCosts(i,i) = 0;
+    pairwiseCosts(i-1,i) = 2;
+    pairwiseCosts(i,i-1) = 2;
+    pairwiseCosts(i-2,i) = 2;
+    pairwiseCosts(i,i-2) = 2;
+end
+pairwiseCosts(1,1)=0;
+pairwiseCosts(2,2)=0;
+pairwiseCosts(1,2)=2;
+pairwiseCosts(2,1)=2;
+
 %Now, use dynamic programming to find best solution.
 %TO DO - fill in this routine (further down in this file).
 bestPath = dynamicProgram(unaryCosts,pairwiseCosts);
@@ -55,7 +69,16 @@ bestPathCostPairwise = sum(pairwiseCosts(bestPath(1:end-1)+nY*(bestPath(2:end)-1
 bestCost = bestPathCostUnary+bestPathCostPairwise;
 fprintf('Path Cost = %3.3f\n',bestCost);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Compose!
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-bestPath
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+notecreate = @(frq,dur) sin(2*pi* [1:dur]/8192 * (440*2.^((frq-1)/12)));
+notename = {'A' 'A#' 'B' 'C' 'C#' 'D' 'D#' 'E' 'F' 'F#' 'G' 'G#'};
+
+songidx = bestPath;
+dur = 0.3*8192;
+songnote = [];
+for k1 = 1:length(songidx)
+    songnote = [songnote; [notecreate(songidx(k1),dur)  zeros(1,75)]'];
+end
+soundsc(songnote, 8192)
